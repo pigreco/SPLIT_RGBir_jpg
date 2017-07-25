93644863aedd232604be9e69248dafbe9350dec0
Salvatore <pigrecoinfinito@gmail.com>
2017-07-25 15:09:03 +0200

    Update script_1.sh

--- script_1.sh
+++ script_1.sh
@@ -16,5 +16,5 @@ for i in *.jpg;
      gdal_translate -b 1  ./"$i" ./RGB/"$filename"_R.jpg;
      gdal_translate -b 2  ./"$i" ./RGB/"$filename"_G.jpg;
      gdal_translate -b 3  ./"$i" ./RGB/"$filename"_B.jpg;
-     gdal_translate -b 3  ./"$i" ./ir/"$filename"_IR.jpg;
+     gdal_translate -b 4  ./"$i" ./ir/"$filename"_IR.jpg;
  done
