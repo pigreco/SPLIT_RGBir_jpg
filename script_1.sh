93cb4483b0d647d52fe1115a362996ae881522d1
Salvatore <pigrecoinfinito@gmail.com>
2017-07-25 14:40:30 +0200

    Create script_1.sh

---
+++ script_1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+#creo una cartella di output
+mkdir ./RGB
+
+for i in *.jpg; 
+ do 
+  #crei una variabile che usi per estrarre nome e estensione
+  filename=$(basename "$i")
+  #estrai estensione
+  extension="${filename##*.}"
+  #estrai nome file
+  filename="${filename%.*}" 
+     gdal_translate -b 1  ./"$i" ./RGB/"$filename"_R.jpg;
+     gdal_translate -b 2  ./"$i" ./RGB/"$filename"_G.jpg;
+     gdal_translate -b 3  ./"$i" ./RGB/"$filename"_B.jpg;
+ done
